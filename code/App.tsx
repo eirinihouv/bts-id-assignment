86289daecf2111dd3c228da485745fe1d8a8ca41
Irene Houvarda <eirinihouv@hotmail.com>
2020-04-20 22:29:37 +0300

    mobile page and scroll completed

--- code/App.tsx
+++ code/App.tsx
@@ -1,12 +1,13 @@
 import * as React from "react"
-import { Override, Data } from "framer"
+import { Data, Override, Scroll } from "framer"
 
-// Learn more: https://framer.com/docs/overrides/
-
-const appState = Data({
+const state = Data({
     taps: 0,
+    page: 0,
+    max: 0,
 })
 
+//primarybutton onTap
 export function TapFrame(props): Override {
     return {
         borderRadius: 12,
@@ -14,21 +15,65 @@ export function TapFrame(props): Override {
             scale: 0.9,
         },
         onTap: () => {
-            appState.taps += 1
+            state.taps += 1
         },
     }
 }
 
-export function RotateFrame(props): Override {
+//bottom-nav
+export function DesktopPage(props): Override {
+    const component = props.children[0]
+    const pages = component.props.children
+    state.max = pages.length - 1
     return {
-        animate: {
-            rotate: appState.taps * 90,
+        currentPage: state.page,
+        onChangePage: index => {
+            state.page = index
         },
     }
 }
 
-export function TextElement(props): Override {
+export function ButtonNextPage(): Override {
     return {
-        text: appState.taps,
+        onClick: () => {
+            if (state.page === state.max) {
+                state.page = 0
+            } else {
+                state.page += 1
+            }
+        },
     }
+}
+
+export function ButtonPreviousPage(): Override {
+    return {
+        onClick: () => {
+            state.page -= 1
+        },
+    }
+}
+
+//double scroll
+export function DoubleScroll(): Override {
+    return <Scroll direction="both" directionLock={false}></Scroll>
+}
+
+//accordion
+export function Accordion(props): Override {
+    return {
+        variants: {
+            open: {
+                height: 207,
+            },
+            closed: {
+                height: 50,
+            },
+        },
+        initial: "closed",
+        whileTap: "open",
+    }
+}
+
+export function Item(props): Override {
+    return {}
 }
--- code/canvas.tsx
+++ code/canvas.tsx
@@ -1,13 +1,25 @@
 // WARNING: this file is auto generated, any changes will be lost
 import { createDesignComponent, CanvasStore } from "framer"
 const canvas = CanvasStore.shared(); // CANVAS_DATA;
-export const Bottom_nav = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_daC_XOnuv", {}, 21,10);
+export const BurgerMenu = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_z28SDS_TP", {}, 20,12);
 export const Button = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_u2DdvobrU", {}, 198,40);
-export const Description = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_iT6ZB435H", {}, 456,131);
-export const Menuitem_Skateboards = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_Tp3ssBMHY", {}, 125,19);
-export const Menuitem_clothing = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_bYByQkHaU", {}, 88,19);
-export const Menuitem_longboards = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_FrY3lwrYj", {}, 132,21);
-export const Price = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_pjcduiBSt", {}, 150,30);
+export const Carver_back = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_n8UzlXsUF", {}, 212,566);
+export const Carver_back_1 = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_aVtd_Y7En", {}, 62,166);
+export const Carver_front = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_qs9nIeG1M", {}, 175,519);
+export const Carver_front_1 = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_vX4cCRWrD", {}, 54,159);
+export const Desktop_item_display = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_je62gIZ5K", {}, 1165,538);
+export const Item = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_EkmTj8MlG", {}, 196,50);
+export const Item_active = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_bDaB2cMSy", {}, 196,50);
+export const MainNav = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_T6PjuMXvZ", {}, 1165,140);
+export const Menu = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_oTwAmMv8h", {}, 183,21);
+export const Mobile_item_description = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_jTgLj9K72", {}, 375,675);
+export const Mobile_item_display = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_AHkUAdl3r", {}, 375,675);
 export const Shopping_cart = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_wNxiCpLE9", {}, 44,45);
 export const Shopping_cart_empty = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_zglUFdUct", {}, 44,45);
 export const Shopping_cart_empty_1 = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_JQQegxpwV", {}, 44,45);
+export const Side_menu = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_geoR6U_eu", {}, 275,538);
+export const Submenu = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_lsQ9xPaRM", {}, 183,21);
+export const Submenu_active = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_JWyERM7ar", {}, 183,21);
+export const Top_menu_item = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_VuZ4o3X2c", {}, 92,22);
+export const Top_menu_item_active = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_n_E3YCzAD", {}, 129,22);
+export const Top_nav = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_Ns6Zlobql", {}, 375,92);
